405925e5718aadf0366bfbbab6b7c1a5887e2764
Simon Pfreundschuh <simonpf@chalmers.se>
2016-08-11 14:15:31 +0200

    Removed unused code in OpenCL.cxx

--- tmva/tmva/src/DNN/Architectures/OpenCL.cxx
+++ tmva/tmva/src/DNN/Architectures/OpenCL.cxx
@@ -33,54 +33,5 @@ std::shared_ptr<TOpenCLDevice> TOpenCL::GetDefaultDevice()
    }
 }
 
-
-// template <>
-// void TDeviceDataLoader<TOpenCL, MatrixInput_t>::CopyBatch(
-//     const MatrixInput_t & data,
-//     IndexIterator_t sampleIndexIteratorBegin,
-//     size_t batchSize,
-//     TOpenCLDevice::HostBuffer_t & buffer)
-// {
-//    const TMatrixT<Double_t> &inputData  = std::get<0>(data);
-//    const TMatrixT<Double_t> &outputData = std::get<1>(data);
-
-//    size_t nInput  = inputData.GetNcols();
-//    size_t nOutput = outputData.GetNcols();
-
-//    // Copy input matrix;
-
-//    IndexIterator_t sampleIndexIterator = sampleIndexIteratorBegin;
-//    for (size_t i = 0; i < batchSize; i++) {
-//        size_t sampleIndex = *sampleIndexIterator;
-//        // Copy input matrices.
-//        for (size_t j = 0; j < nInput; j++) {
-//            size_t bufferIndex = j * batchSize + i;
-//            buffer[bufferIndex] = inputData(sampleIndex, j);
-//        }
-//        sampleIndexIterator++;
-//    }
-
-//    // Copy output matrix;
-
-//    size_t offset = nInput * batchSize;
-//    sampleIndexIterator = sampleIndexIteratorBegin;
-//    for (size_t i = 0; i < batchSize; i++) {
-//        size_t sampleIndex = *sampleIndexIterator;
-//        // Copy input matrices
-//        for (size_t j = 0; j < nOutput; j++) {
-//            size_t bufferIndex = j * batchSize + i;
-//            buffer[offset + bufferIndex] = outputData(sampleIndex, j);
-//        }
-//        sampleIndexIterator++;
-//    }
-// }
-
-// template
-// void TDeviceDataLoader<TOpenCL, MatrixInput_t>::CopyBatch(
-//     const MatrixInput_t & data,
-//     IndexIterator_t sampleIndexIteratorBegin,
-//     size_t batchSize,
-//     TOpenCLDevice::HostBuffer_t & buffer);
-
 } // namespace TMVA
 } // namespace DNN
